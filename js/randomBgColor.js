61430a32da10311bb2348add118997e82bc585fd
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-28 20:15:07 +0600

    randomBgColor.js done

---
+++ js/randomBgColor.js
@@ -0,0 +1,98 @@
+// set and reset random background color
+
+// set and reset triangle card random background color
+
+// set 
+
+document.getElementById('triangle-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('triangle-card');
+});
+
+// reset
+
+document.getElementById('triangle-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('triangle-card');
+});
+
+// set and reset rectangle card random background color
+
+//set
+
+document.getElementById('rectangle-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('rectangle-card');
+});
+
+//reset
+
+document.getElementById('rectangle-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('rectangle-card');
+});
+
+// set and reset parallelogram card random background color 
+
+//set
+
+document.getElementById('parallelogram-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('parallelogram-card');
+});
+
+//reset
+
+document.getElementById('parallelogram-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('parallelogram-card');
+});
+
+// set and reset rhombus card random background color
+
+//set
+
+document.getElementById('rhombus-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('rhombus-card');
+});
+
+//reset
+
+document.getElementById('rhombus-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('rhombus-card');
+});
+
+// set and reset pentagon card random background color
+
+//set
+
+document.getElementById('pentagon-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('pentagon-card');
+});
+
+//reset
+
+document.getElementById('pentagon-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('pentagon-card');
+});
+
+
+// set and reset ellipse card random background color
+
+//set
+
+document.getElementById('ellipse-card').addEventListener('mouseenter',function(){
+
+    randomBgColor('ellipse-card');
+});
+
+//reset
+
+document.getElementById('ellipse-card').addEventListener('mouseleave',function(){
+
+    resetBgColor('ellipse-card');
+});
